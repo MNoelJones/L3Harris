6592fc2ad512802c1b2bc528cc546312eff292fc
Michael Noel Jones <mnjones+git@gmail.com>
2023-02-16 11:38:27 +0000

    Fixes for C++ issues

--- c++/l3harris.cpp
+++ c++/l3harris.cpp
@@ -1,49 +1,74 @@
 #include <filesystem>
-using namespace fs = std::filesystem;
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+namespace fs = std::filesystem;
+using namespace std;
 
 auto get_files(std::string path)
 {
     return fs::directory_iterator(path);
 }
 
-auto get_procs()
-{
-    return get_files("/proc");
+bool is_number(const std::string& s) {
+    return !s.empty() && std::find_if(
+        s.begin(), s.end(),
+        [](unsigned char c) { return !std::isdigit(c); }
+    ) == s.end();
 }
 
-auto get_procs_opendir()
+auto get_procs()
 {
     vector<string> proc_ids;
-    dirp = opendir("/proc");
-    while ((dp = readdir(dirp)) != NULL)
-    {
-        proc_ids.push_back(dp->d_name);
+    for (const auto & entry : get_files("/proc")) {
+        if (entry.is_directory()) {
+
+            if (is_number(entry.path().filename().string())) {
+                proc_ids.push_back(entry.path().filename().string());
+            }
+        }
     }
     return proc_ids;
 }
 
-auto get_mac_address(ifname = "eth0")
+// auto get_procs_opendir()
+// {
+//     vector<string> proc_ids;
+//     auto dirp = opendir("/proc");
+//     while ((dp = readdir(dirp)) != NULL)
+//     {
+//         proc_ids.push_back(dp->d_name);
+//     }
+//     return proc_ids;
+// }
+
+auto get_mac_address(std::string ifname="eth0")
 {
-    const fs::path mac_file{"/sys/class/net"} / ifname / "address";
+    const fs::path mac_file_pre{"/sys/class/net"};
+    fs::path mac_file = mac_file_pre / ifname / "address";
     std::ifstream f(mac_file, std::ios::in);
     const auto size = fs::file_size(mac_file);
     std::string result(size, '\0');
-    f.read(result.data(), sz);
+    f.read(result.data(), size);
     return result;
 }
 
 int main(void)
 {
-    files = get_files(".");
+    auto files = get_files(".");
     for (const auto &entry : files)
     {
         cout << entry.path() << endl;
     }
-    procs = get_procs();
+    auto procs = get_procs();
     for (const auto &entry : procs)
     {
-        cout << entry.filename() << endl;
+        cout << entry << endl;
     }
-    auto mac_addr = get_mac_address();
+    auto mac_addr = get_mac_address("enp0s3");
     cout << mac_addr << endl;
 }
